92cc107ab6ec8cee9847d1bae425d2de01b7243f
Lucas <lucas@plannerti.com.br>
2023-06-27 10:54:19 -0300

    Realizando dele, erro

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -11,17 +11,17 @@ namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
-        private readonly SellerService _sellerServivice;
+        private readonly SellerService _sellerService;
         private readonly DepartmentService _departmentService;
 
         public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
-            _sellerServivice = sellerService;
+            _sellerService = sellerService;
             _departmentService = departmentService;
         }
         public IActionResult Index()
         {
-            var list = _sellerServivice.FindAll();
+            var list = _sellerService.FindAll();
 
             return View(list);
         }
@@ -32,13 +32,30 @@ namespace SalesWebMvc.Controllers
             var viewModel = new SellerFormViewModel { Departments = departments };
             return View(viewModel);
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
-            _sellerServivice.Insert(seller);
+            _sellerService.Insert(seller);
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
         }
     }
 }
