4e10e68439b2bd1d6f87f66f84c15cf12ae52bd6
Lucas <lucas@plannerti.com.br>
2023-07-05 09:58:49 -0300

    Implementing grouping search

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -25,7 +25,7 @@ namespace SalesWebMvc.Models
             Sellers.Add(seller);
         }
 
-        public double TotalSaller(DateTime initial, DateTime final)
+        public double TotalSales(DateTime initial, DateTime final)
         {
             return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
